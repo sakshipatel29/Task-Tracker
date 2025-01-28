2a246b606822a385b582e693529c6d4e34b38a7d
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-01-27 23:03:04 -0600

    Error Message and field requirements added for better experience.

--- task/src/components/AddTask.jsx
+++ task/src/components/AddTask.jsx
@@ -5,21 +5,32 @@ const AddTask = ({taskList, setTaskList}) => {
     const [addModal, setAddModal] = useState(false);
     const [projectName, setProjectName] = useState("");
     const [taskDescription, setTaskDescription] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleInput = (e) => {
         let {name, value} = e.target;
 
-        if (name === "projectName") setProjectName(value);
+        if (name === "projectName"){ 
+            setProjectName(value);
+            setErrorMessage("");
+        }
+        if (name === "projectName" && name === ""){
+            setErrorMessage("Please enter task name");
+        }
         if (name === "taskDescription") setTaskDescription(value);
     }
 
     const handleAdd = e => {
         e.preventDefault();
-        setTaskList([...taskList, { projectName, taskDescription }]);
+        if(!projectName){
+            setErrorMessage("Please enter task name");
+        }else {
+            setTaskList([...taskList, { projectName, taskDescription }]);
         setProjectName("");
         setTaskDescription("");
         setAddModal(false);
         console.log([...taskList, { projectName, taskDescription }]);
+        }
     }
 
     return (
@@ -37,7 +48,8 @@ const AddTask = ({taskList, setTaskList}) => {
                                 <button onClick={() => {setAddModal(false)}}>X</button>
                                 </div>
                                 <div>
-                                    Task Name: <input type='text' placeholder='a project name' name='projectName' value={projectName} onChange={handleInput}  />
+                                    Task Name: <input type='text' placeholder='a project name' name='projectName' value={projectName} onChange={handleInput} required />
+                                    <p>{errorMessage}</p>
                                     Description:  <textarea type='text' name='taskDescription' value={taskDescription} onChange={handleInput} > </textarea>
                                     <button onClick={handleAdd}>Add</button>
                                 </div>
--- task/src/components/EditTask.jsx
+++ task/src/components/EditTask.jsx
@@ -5,11 +5,18 @@ const EditTask = ({task, index, taskList, setTaskList}) => {
     const [editModal, setEditModal] = useState(false);
     const [projectName, setProjectName] = useState("");
     const [taskDescription, setTaskDescription] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleInput = (e) => {
         let {name, value} = e.target;
 
-        if (name === "projectName") setProjectName(value);
+        if (name === "projectName"){ 
+            setProjectName(value);
+            setErrorMessage("");
+        }
+        if (name === "projectName" && name === ""){
+            setErrorMessage("Please enter task name");
+        }
         if (name === "taskDescription") setTaskDescription(value);
     }
 
@@ -20,11 +27,15 @@ const EditTask = ({task, index, taskList, setTaskList}) => {
 
     const handleUpdate = e => {
         e.preventDefault();
+        if(!projectName){
+            setErrorMessage("Please enter task name");
+        }else {
         let taskIndex = taskList.indexOf(task);
         taskList.splice(taskIndex,1);
         setTaskList([...taskList, { projectName, taskDescription }]);
         setEditModal(false);
         console.log([...taskList, { projectName, taskDescription }]);
+        }
     } 
 
     return (
@@ -42,7 +53,8 @@ const EditTask = ({task, index, taskList, setTaskList}) => {
                                 <button onClick={() => {setEditModal(false)}}>X</button>
                                 </div>
                                 <div>
-                                    Task Name: <input type='text' placeholder='a project name' name='projectName' value={projectName} onChange={handleInput}  />
+                                    Task Name: <input type='text' placeholder='a project name' name='projectName' value={projectName} onChange={handleInput} required />
+                                    <p>{errorMessage}</p>
                                     Description:  <textarea type='text' name='taskDescription' value={taskDescription} onChange={handleInput} > </textarea>
                                     <button onClick={handleUpdate}>Update Task</button>
                                 </div>
