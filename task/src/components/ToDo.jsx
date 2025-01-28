b9de33dc7fd971781846c2090241f59f401828b9
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-01-27 22:49:04 -0600

    Delete function ad.ded to delete tasks

--- task/src/components/ToDo.jsx
+++ task/src/components/ToDo.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import EditTask from './EditTask';
 
 const ToDo = ({task, index, taskList, setTaskList}) => {
-  return (
+
+    const handleDelete = ItemID => {
+        let removeIndex = taskList.indexOf(task);
+        taskList.splice(removeIndex ,1);
+        setTaskList((currentTasks => currentTasks.filter(todo => todo.id !== ItemID)))
+    }
+
+return (
     <div>
         <p>{task.projectName}</p>
         <EditTask task={task} index={index} taskList={taskList} setTaskList={setTaskList}/>
         <p>{task.taskDescription}</p>
-        <button>Delete</button>
+        <button onClick={handleDelete}>Delete</button>
     </div>
-  )
+)
 }
 
 export default ToDo;
